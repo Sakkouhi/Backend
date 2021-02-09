ebcae7de2b2c93689ed0255c8085e2cfaa01b36c
tarek sakkouhi <sakkouhitarek09@gmail.com>
2021-02-09 11:41:19 +0100

    update des routes aprés creation des controlleurs

--- routes/providers.js
+++ routes/providers.js
@@ -2,31 +2,52 @@ const express = require('express');
 const router = express.Router();
 const mongoose = require('mongoose');
 
+const {ProviderController} = require("../controllers/provider");
 
 router.post('/', async (req, res) => {
-  
-  res.status(201).json({
-        message : 'created'
+  const provider = await ProviderController.save(req.body);
+  res.status(201).json(provider);
+});
+
+router.get('/:provider_id', async (req, res) => {
+  if (!mongoose.Types.ObjectId.isValid(req.params.provider_id))
+    res.status(400).json({
+      message: "Invalid ID"
+    });
+  const provider = await ProviderController.findById(req.params.provider_id);
+  if (provider)
+    res.status(200).json(provider);
+  else
+    res.status(404).json({
+      message: "Provider not found"
     });
 });
 
-router.get('/:provider_id',  (req, res) => {
-    const id = req.params.provider_id;
-    res.status(200).json({
-        id: id
+router.delete('/:provider_id', async (req, res) => {
+  if (!mongoose.Types.ObjectId.isValid(req.params.provider_id))
+    res.status(400).json({
+      message: "Invalid ID"
     });
-    
+  const provider = await ProviderController.deleteOne({_id: req.params.provider_id});
+  if (provider)
+    res.status(201).json(provider);
+  else
+    res.status(404).json({
+      message: "Provider not found"
+    });
 });
 
-router.delete('/:provider_id',  (req, res) => {
-    res.status(200).json({
-        message : 'deleated'
+router.put('/:provider_id', async (req, res) => {
+  if (!mongoose.Types.ObjectId.isValid(req.params.provider_id))
+    res.status(400).json({
+      message: "Invalid ID"
     });
-});
-
-router.put('/:provider_id',  (req, res) => {
-    res.status(200).json({
-        message : 'updated'
+  const provider = await ProviderController.findOneAndUpdate(req.params.provider_id, req.body);
+  if (provider)
+    res.status(200).json(provider);
+  else
+    res.status(404).json({
+      message: "Provider not found"
     });
 })
 
